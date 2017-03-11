ecf6d5abdc3c77b18fce393467558caf41c209f3
Aktorius <miri.tarik@gmail.com>
2017-03-11 14:54:55 +0000

    implement business logic for the tips management

--- app/src/main/java/aktorius/com/android/tipcalculator/MainActivity.java
+++ app/src/main/java/aktorius/com/android/tipcalculator/MainActivity.java
@@ -2,12 +2,63 @@ package aktorius.com.android.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.editTextBillAmount)
+    EditText editBillAmount;
+    @BindView(R.id.tvTipPercent)
+    TextView tipPercent;
+    @BindView(R.id.tvTipTotal)
+    TextView tipAmount;
+    @BindView(R.id.tvBillTotalAmount)
+    TextView billTotalAmount;
+
+    private float percentage = 0;
+    private float tipTotal = 0;
+    private float finalBillAount = 0;
+
+    private final float DEFAULT_TIP_PERCENT = 15;
+    private final float REGULAR_TIP_PERCENT = 10;
+    private final float EXCELLENT_TIP_PERCENT = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setTipValues();
+    }
+
+    private void setTipValues() {
+
+        tipPercent.setText(getString(R.string.main_msg_tippercent, percentage));
+        tipAmount.setText(getString(R.string.main_msg_tiptotal, tipTotal));
+        billTotalAmount.setText(getString(R.string.main_msg_billtotalresult, finalBillAount));
+    }
+
+    @OnClick({R.id.regularService, R.id.goodService, R.id.excellentService})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.regularService:
+                percentage = REGULAR_TIP_PERCENT;
+                break;
+            case R.id.goodService:
+                percentage = DEFAULT_TIP_PERCENT;
+                break;
+            case R.id.excellentService:
+                percentage = EXCELLENT_TIP_PERCENT;
+                break;
+            default:
+                percentage = 0;
+        }
     }
 }
